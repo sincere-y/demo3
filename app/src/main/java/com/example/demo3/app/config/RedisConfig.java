8d6a116d8f5d431a117e0008bbb90569e3a07a77
14642 <1464285392@qq.com>
2025-05-10 00:10:54 +0800

    更改

--- app/src/main/java/com/example/demo3/app/config/RedisConfig.java
+++ app/src/main/java/com/example/demo3/app/config/RedisConfig.java
@@ -4,6 +4,8 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisSentinelConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 
@@ -11,6 +13,20 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 @EnableCaching
 public class RedisConfig {
+    @Bean
+    public RedisSentinelConfiguration sentinelConfig() {
+        RedisSentinelConfiguration sentinelConfig = new RedisSentinelConfiguration()
+                .master("master")
+                .sentinel("127.0.0.1", 26379)
+                .sentinel("127.0.0.1", 26479)
+                .sentinel("127.0.0.1", 26579);
+        return sentinelConfig;
+    }
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(sentinelConfig());
+    }
     @Bean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
